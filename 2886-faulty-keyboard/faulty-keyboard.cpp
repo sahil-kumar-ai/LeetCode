ba7096e72a98c3ec1659ac1bbbb1e8922d857622
Sahil Kumar <sahilkumar2212007@gmail.com>
2026-08-25 20:42:13 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (62.23%) - LeetSync

---
+++ 2886-faulty-keyboard/faulty-keyboard.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string finalString(string s) {
+        string res = "";
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == 'i') {
+                reverse(res.begin(), res.end());
+            } else {
+                res += s[i];
+            }
+        }
+
+        return res;
+    }
+};
