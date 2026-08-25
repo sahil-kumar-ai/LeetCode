2380d55a208fc09bd086208242e515531ceaf8c0
Sahil Kumar <sahilkumar2212007@gmail.com>
2026-08-25 20:54:22 +0530

    Time: 2 ms (6.30%) | Memory: 8.2 MB (97.67%) - LeetSync

---
+++ 2401-count-asterisks/count-asterisks.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countAsterisks(string s) {
+        bool inside = false;
+        int count = 0;
+
+        for (char c : s) {
+            if (c == '|') inside = !inside;
+            else if (c == '*' && !inside) count++;
+        }
+
+        return count;
+    }
+};
